ee1069a2b067387e734ebae4b04e51769b844415
Kadell <Kadell@MacBook.local>
2016-12-01 14:31:14 -0500

    Project File

---
+++ NYT-Quint-A/NYT-Quint-A/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NYT-Quint-A
+//
+//  Created by Kadell on 12/1/16.
+//  Copyright © 2016 Kadell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
